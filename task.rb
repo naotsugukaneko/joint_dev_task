3b1ee675445b1b9d949c17713b3bea6929edbf2f
naotsugukaneko <nkk.407@gmail.com>
2021-02-08 19:43:16 +0900

    Q7,Q8,Q10を修正しました

--- task.rb
+++ task.rb
@@ -57,7 +57,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  array.map! { |x| x.to_i}
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -66,8 +66,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map { |lang| lang.capitalize!}
-  upper_case_programming_languages = programming_languages.map { |lang| lang.upcase}
+  programming_languages.map(&:capitalize!)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -86,10 +86,12 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-  if foods.include?("うに")
-    puts "好物です"
-  else
-    puts "まぁまぁ好きです"
+  foods.each do |food|
+    if food.include?("うに")
+     puts "#{food}:好物です"
+    else
+     puts "#{food}:まぁまぁ好きです"
+    end
   end
 end
 
